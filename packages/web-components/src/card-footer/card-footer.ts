d6318e2207295531d96176b63ed1d2b4f75c74f5
brianchristopherbrady <brian.christopher.brady@gmail.com>
2023-08-28 09:41:57 -0700

    card: removes dead code

--- packages/web-components/src/card-footer/card-footer.ts
+++
@@ -1,9 +0,0 @@
-import { FASTElement } from '@microsoft/fast-element';
-
-/**
- * @class CardFooter component
- *
- * @remarks
- * This class extends FASTElement. a content container used in the Card component
- */
-export class CardFooter extends FASTElement {}
--- packages/web-components/src/card-header/card-header.ts
+++
@@ -1,9 +0,0 @@
-import { FASTElement } from '@microsoft/fast-element';
-
-/**
- * @class Card Header component
- *
- * @remarks
- * This class extends HTMLElement
- */
-export class CardHeader extends FASTElement {}
